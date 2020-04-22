6bf3b27d32d9a04b702300ad88e8f74af8c0d90c
宇薪 邱 <simoncat524@hotmail.com>
2020-04-23 00:08:23 +0800

    至進度 2_01_10

---
+++ Iceland_Moss/Iceland_Moss/Model/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Iceland_Moss.Model
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public string HeroColor { get; set; }
+        public bool IsFeatured { get; set; }
+
+        //todo - 
+    }
+}
--- Iceland_Moss/Iceland_Moss/ViewModels/MainPageViewModel.cs
+++ Iceland_Moss/Iceland_Moss/ViewModels/MainPageViewModel.cs
@@ -1,8 +1,10 @@
-﻿using Prism.Commands;
+﻿using Iceland_Moss.Model;
+using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 
@@ -10,10 +12,54 @@ namespace Iceland_Moss.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
+        private IList<Product> _products;
+        public IList<Product> Products
+        {
+            get { return _products; }
+            set { SetProperty(ref _products, value); }
+        }
+
         public MainPageViewModel(INavigationService navigationService)
             : base(navigationService)
         {
             Title = "Main Page";
+
+            Products = new ObservableCollection<Product>()
+            {
+                new Product()
+                {
+                     Name="Sky Blue",
+                     HeroColor="#96C9F8",
+                     ImageUrl="",
+                     Price=12,
+                     IsFeatured=false
+                },
+                new Product()
+                {
+                     Name="Lavander",
+                     HeroColor="#D69EFC",
+                     ImageUrl=" ",
+                     Price=19,
+                     IsFeatured=false
+                },
+                new Product()
+                {
+                     Name="Yellow Sun",
+                     HeroColor="#FFCA81",
+                     ImageUrl=" ",
+                     Price=17,
+                     IsFeatured=false
+                },
+                new Product()
+                {
+                     Name="Green Life ",
+                     HeroColor="#74D69E",
+                     ImageUrl=" ",
+                     Price=14,
+                     IsFeatured=false
+                }
+
+        };
         }
     }
 }
