47139b837c9fad757c971b5b75fcdc130269835d
FocacciaSyin <simoncat524@hotmail.com>
2020-10-03 11:36:13 +0800

    購物車可顯示 圖片.數量.金額等資訊

--- Iceland_Moss/Iceland_Moss/ViewModels/MainPageViewModel.cs
+++ Iceland_Moss/Iceland_Moss/ViewModels/MainPageViewModel.cs
@@ -34,13 +34,13 @@ namespace Iceland_Moss.ViewModels
 
             Products = new ObservableCollection<Product>()
             {
-                new Product(){Sort=1,Name="Sky Blue",HeroColor="#96C9F8",ImageUrl="",Price=12,IsFeatured=false},
-                new Product(){Sort=2,Name="Lavander",HeroColor="#D69EFC",ImageUrl="",Price=19,IsFeatured=true},
-                new Product(){Sort=3,Name="Yellow Sun",HeroColor="#FFCA81",ImageUrl="",Price=17,IsFeatured=true},
-                new Product(){Sort=4,Name="Green Life ",HeroColor="#74D69E",ImageUrl="",Price=14,IsFeatured=false},
-                new Product(){Sort=5,Name="Red Life ",HeroColor="#DC3333",ImageUrl="",Price=12,IsFeatured=true},
-                new Product(){Sort=6,Name="Orange Life ",HeroColor="#F4BA51",ImageUrl="",Price=10,IsFeatured=false},
-                new Product(){Sort=7,Name="Pink Life ",HeroColor="#FCA4B4",ImageUrl="",Price=10,IsFeatured=false},
+                new Product(){Sort=1,Name="Sky Blue",HeroColor="#96C9F8",ImageUrl="moss.png",Price=12,IsFeatured=false},
+                new Product(){Sort=2,Name="Lavander",HeroColor="#D69EFC",ImageUrl="moss.png",Price=19,IsFeatured=true},
+                new Product(){Sort=3,Name="Yellow Sun",HeroColor="#FFCA81",ImageUrl="moss.png",Price=17,IsFeatured=true},
+                new Product(){Sort=4,Name="Green Life ",HeroColor="#74D69E",ImageUrl="moss.png",Price=14,IsFeatured=false},
+                new Product(){Sort=5,Name="Red Life ",HeroColor="#DC3333",ImageUrl="moss.png",Price=12,IsFeatured=true},
+                new Product(){Sort=6,Name="Orange Life ",HeroColor="#F4BA51",ImageUrl="moss.png",Price=10,IsFeatured=false},
+                new Product(){Sort=7,Name="Pink Life ",HeroColor="#FCA4B4",ImageUrl="moss.png",Price=10,IsFeatured=false},
             };
 
             ShoppingCart = new ShoppingCartViewModel();
